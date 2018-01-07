2bab2e25529d9b32a471b0a0a876c37095059701
GREGA-AW\grega <grega@mohorko.info>
2018-01-07 17:46:34 +0100

    Signing and license.

--- src/BlueWSClient.NET/BlueWSClient.NET/Properties/AssemblyInfo.cs
+++ src/BlueWSClient.NET/BlueWSClient.NET/Properties/AssemblyInfo.cs
@@ -1,15 +1,35 @@
-﻿using System.Reflection;
+﻿/*
+   Copyright 2018 Grega Mohorko
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+Project: BlueWSClient.NET
+Created: 2018-1-7
+Author: GregaMohorko
+*/
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BlueWSClient.NET")]
+[assembly: AssemblyTitle("BlueWS")]
 [assembly: AssemblyDescription("A client library for BlueWS library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BlueWSClient.NET")]
+[assembly: AssemblyCompany("Grega Mohorko")]
+[assembly: AssemblyProduct("BlueWS")]
 [assembly: AssemblyCopyright("Copyright © Grega Mohorko 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
